a9cd7da51029bd77c67ddcd823a0ebdd80d2f745
IramGillani <iramgillani786@outlook.com>
2025-08-11 07:28:52 +0500

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { navLinks } from "../../constants";
+
+const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: { trigger: "nav", start: "bottom top" },
+    });
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        ease: "power1.inOut",
+        duration: 1,
+      }
+    );
+  });
+  return (
+    <nav>
+      <div>
+        <a href="#" className="flex items-center gap-2">
+          <img src="./images/logo.png" alt="logo" />
+          <p>Mojito</p>
+        </a>
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

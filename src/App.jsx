152dd0db9e2671d8a013536cfadbcdde82435873
IramGillani <iramgillani786@outlook.com>
2025-08-12 10:53:26 +0500

    implement first part of hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <section className="py-96 bg-red-200"></section>
     </main>
   );
 }
